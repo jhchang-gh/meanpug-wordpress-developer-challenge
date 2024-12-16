e190d9eb6b0d86b91d86749d276df026f2a8ab56
Jeff Chang <tsavij@gmail.com>
2024-12-16 14:18:18 -0500

    configure challenge for use on macos architecture

---
+++ challenge-1/theme/page-challenge-1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Challenge 1
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header();
+
+?>
+
+<div class="flex">
+	<h1 class="text-3xl font-bold text-red-900">Hello World!!</h1>
+</div>
+
+<?php
+
+get_footer();
+
+
